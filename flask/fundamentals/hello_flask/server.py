e271d4b96d9b2f069829f421628e883393da7185
sngrass0 <sngrasso23@gmail.com>
2023-01-12 11:18:25 -0800

    hello update

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -1,10 +1,10 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template # Import Flask to allow us to create our app
 
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template('index.html', phrase="hello", times=10)  # Return the string 'Hello World!' as a response
 
 # import statements, maybe some other routes
 @app.route('/success')
@@ -21,6 +21,18 @@ def show_user_profile(username, id):
     print(username)
     print(id)
     return "username: " + username + ", id: " + id
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+       {'name' : 'Michael', 'age' : 35},
+       {'name' : 'John', 'age' : 30 },
+       {'name' : 'Mark', 'age' : 25},
+       {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
     
 # app.run(debug=True) should be the very last statement! 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
