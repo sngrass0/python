9bcfb9d832a882d4dd0d1bea3370ae059905aabe
sngrass0 <sngrasso23@gmail.com>
2023-01-11 21:57:14 -0800

    hello flask

---
+++ flask/fundamentals/hello_flask/server.py
@@ -0,0 +1,27 @@
+from flask import Flask  # Import Flask to allow us to create our app
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+# import statements, maybe some other routes
+@app.route('/success')
+def success():
+  return "success"
+
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+    
+# app.run(debug=True) should be the very last statement! 
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
