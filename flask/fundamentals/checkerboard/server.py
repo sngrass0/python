d656ee2cfabdba69c6da03dda47d954bb4eab555
sngrass0 <sngrasso23@gmail.com>
2023-01-12 21:08:35 -0800

    update checkerboard

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def default():
+def index():
     return render_template('index.html', x = 4, y = 4, color1 = 'green', color2 = 'beige')
 
 @app.route('/<int:x>')
@@ -13,6 +13,10 @@ def board_x(x):
 @app.route('/<int:x>/<int:y>')
 def board_x_y(x,y):
     return render_template('index.html', x = x, y = y, color1 = 'green', color2 = 'beige')
+
+@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def board_color(x,y,color1,color2):
+    return render_template('index.html', x = x, y = y, color1 = color1, color2 = color2)
 
 if __name__ == "__main__":
     app.run(debug=True)
