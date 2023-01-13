51d32b9b3bc4e012524b44c399d51549de2d79ac
sngrass0 <sngrasso23@gmail.com>
2023-01-13 12:22:06 -0800

    upload counter

---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, session
+
+app = Flask(__name__)
+app.secret_key = "In the name of the moon I'll punish YOU"
+
+@app.route('/')
+def index():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 1
+
+    if 'views' in session:
+        session['views'] += 1
+    else:
+        session['views'] = 1
+     # return rendered template with new count
+    return render_template('index.html', count = session['count'], views = session['views'])
+
+@app.route('/destroy_session')
+def destroy_session():
+    # clear all keys in session
+    session.pop('count')
+    return redirect('/')
+
+@app.route('/add_2')
+def add_2():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count']
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
